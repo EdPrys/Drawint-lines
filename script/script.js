0f88c50ba6921b301c44097f011b9b8fa4dbdeed
EdPrys <eduard.prystupa@gmail.com>
2023-06-29 12:39:30 +0300

    fix: midle squere is usefull now

--- script/script.js
+++ script/script.js
@@ -60,10 +60,11 @@ const getClientOffset = (event) => {
   };
 };
 
-const drawLine = (start, end) => {
+const drawLine = (start, mid, end) => {
   context.strokeStyle = "#ffffff";
   context.beginPath();
   context.moveTo(start.x, start.y);
+  context.lineTo(mid.x, mid.y);
   context.lineTo(end.x, end.y);
   context.stroke();
 };
@@ -73,12 +74,22 @@ const drawSquare = (position) => {
   context.fillRect(position.x - 2.5, position.y - 2.5, 5, 5);
 };
 
+const calculateMidPoint = (start, end) => {
+  return {
+    x: (start.x + end.x) / 2,
+    y: (start.y + end.y) / 2,
+  };
+};
+
 const mouseDownListener = (event) => {
   const position = getClientOffset(event);
 
   // Перевіряємо, чи клацнули на квадратик
   for (const line of lines) {
-    for (const square of [line.start, line.end]) {
+    const { start, mid, end } = line;
+    const squares = [start, mid, end];
+
+    for (const square of squares) {
       const dx = square.x - position.x;
       const dy = square.y - position.y;
       if (Math.sqrt(dx * dx + dy * dy) < 2.5) {
@@ -91,7 +102,7 @@ const mouseDownListener = (event) => {
 
   // Якщо не клацнули на квадратик, створюємо нову лінію
   isDrawStart = true;
-  const newLine = { start: position, end: position };
+  const newLine = { start: position, mid: position, end: position };
   lines.push(newLine);
   selectedLine = newLine;
   drawSquare(position);
@@ -101,23 +112,30 @@ const mouseMoveListener = (event) => {
   if (isDrawStart && selectedLine) {
     const position = getClientOffset(event);
     selectedLine.end = position;
+    selectedLine.mid = calculateMidPoint(selectedLine.start, position);
   } else if (selectedSquare) {
     const position = getClientOffset(event);
     selectedSquare.x = position.x;
     selectedSquare.y = position.y;
+
+    if (selectedLine.mid === selectedSquare) {
+      const dx = position.x - selectedSquare.x;
+      const dy = position.y - selectedSquare.y;
+      selectedLine.start.x += dx;
+      selectedLine.start.y += dy;
+      selectedLine.mid.x += dx;
+      selectedLine.mid.y += dy;
+      selectedLine.end.x += dx;
+      selectedLine.end.y += dy;
+    }
   }
 
   clearCanvas();
   for (const line of lines) {
-    drawLine(line.start, line.end);
+    drawLine(line.start, line.mid, line.end);
     drawSquare(line.start);
+    drawSquare(line.mid);
     drawSquare(line.end);
-
-    const midPoint = {
-      x: (line.start.x + line.end.x) / 2,
-      y: (line.start.y + line.end.y) / 2,
-    };
-    drawSquare(midPoint);
   }
 };
 
